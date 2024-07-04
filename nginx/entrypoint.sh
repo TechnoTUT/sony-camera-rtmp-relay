63ab583a6748da15548bce639f3c5db2c9c3afe7
DDlia <gh@ddlia.com>
2024-07-04 19:39:35 +0900

    Change: Make RTMP destination URL dynamically configurable

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sed -i "s|rtmp://10.33.0.1:1935/live/test|$SERVER_URL|g" /etc/nginx/nginx.conf
 echo "Stream Destination: $SERVER_URL"
-nginx -g "daemon off;"
+
+if [ -z "${SERVER_IP}" ]; then
+  echo "Error: SERVER_IP is not set"
+  exit 1
+fi
+
+envsubst '${SERVER_IP}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+nginx -g 'daemon off;'
