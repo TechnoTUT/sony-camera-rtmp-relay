924a0db4b9ab6bc08d6ed4c399bf1c80b967627d
DDlia <gh@ddlia.com>
2024-07-04 19:59:16 +0900

    Fix: Correct variable configuration mistake

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Stream Destination: $SERVER_URL"
 
-if [ -z "${SERVER_IP}" ]; then
-  echo "Error: SERVER_IP is not set"
+if [ -z "${SERVER_URL}" ]; then
+  echo "Error: SERVER_URL is not set"
   exit 1
 fi
 
-envsubst '${SERVER_IP}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${SERVER_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
