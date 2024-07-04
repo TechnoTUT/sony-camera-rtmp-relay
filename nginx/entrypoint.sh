f3c8edcd8e0869366a0e87a90f9d228d1a2903a2
DDlia <gh@ddlia.com>
2024-07-04 09:26:31 +0900

    Fix: Correct nginx arguments

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s|rtmp://10.33.0.1:1935/live/test|$SERVER_URL|g" /etc/nginx/nginx.conf
-echo "$SERVER_URL"
-nginx -g daemon off
+echo "Stream Destination: $SERVER_URL"
+nginx -g "daemon off;"
